e0757c3d01a5e2332688ba65744ffc034a6e470f
wang yuanhang <yuanhangw.sg@gmail.com>
2013-01-06 17:34:38 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dandelion::Application.config.secret_token = '222f4a757b3ad1a0698b78a9903adaf85ce3b6bfb6cc2b0e4428d6f06180d49640630cdd73a6e80a048a1c885fae441ef14baccd6bf1b28b0afb6250db5fd228'
